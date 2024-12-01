48ac2d0659d17509bf44937fc33bdb93125c72b6
최세은 <156633780+CuteSeeun@users.noreply.github.com>
2024-12-01 11:13:32 +0900

    Chore : isseueController.js에서 Request 타입 확장 에러 수정

--- backend/dist/routes/userRouter.js
+++ backend/dist/routes/userRouter.js
@@ -7,6 +7,8 @@ const express_1 = __importDefault(require("express"));
 const userController_1 = require("../controller/userController");
 const kakaoController_1 = require("../controller/kakaoController");
 const phoneController_1 = require("../controller/phoneController");
+const passwdController_1 = require("../controller/passwdController");
+const profileController_1 = require("../controller/profileController");
 // const express = require('express');
 const router = express_1.default.Router();
 // 로그인 회원가입 사용자 유지
@@ -22,4 +24,12 @@ router.post('/checkEmail', userController_1.checkEmail);
 router.post('/checkPhone', phoneController_1.checkPhone);
 router.post('/auth/sendverification', phoneController_1.sendPhoneVerification);
 router.post('/auth/verifyPhone', phoneController_1.verifyPhoneCode);
+//비밀번호 찾기 라우트(로그인,회원가입 쪽)
+router.post('/findPass', passwdController_1.findUserPassword);
+router.post('/vaildaeUser', passwdController_1.vaildateUser);
+//프로필 변경 라우트
+router.post('/user/profile', profileController_1.updateProfile);
+//비밀번호 변경 라우트(프로필 쪽)
+router.post('/check-password', passwdController_1.checkCurrentPw);
+router.post('/change-password', passwdController_1.changePw);
 exports.default = router;
